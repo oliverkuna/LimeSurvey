2ff634d7551050998f4934d15cc9b7cfc054b9c0
Patrick Teichmann <patrick.teichmann@limesurvey.org>
2023-09-05 10:27:10 +0200

    Fixed issue #CR-1320: comfort update message overlapping with dropdown actions and can not be muted for a session

--- assets/packages/comfort_update/comfort_update.js
+++ assets/packages/comfort_update/comfort_update.js
@@ -1,9 +1,6 @@
 'use strict';
 document.addEventListener('DOMContentLoaded', function () {
     $('#update-alert').on('closed.bs.alert', function () {
-        if (!$(this).hasClass('unstable-update')) {
-            $('.update-small-notification').removeClass('hidden');
-        }
         $.ajax({
             url: $(this).attr('data-url-notification-state'),
             type: 'GET',
